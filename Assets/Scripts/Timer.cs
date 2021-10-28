f35446b848af1e62dc50d657617a287a60a060af
michaeladizon <michaela_dizon@dlsu.edu.ph>
2021-10-28 18:51:15 +0800

    added initial timer and modal setup

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    public Text timerText;
+    private float startTime;
+    private bool completed = false;
+    public Text finalTime;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        startTime = Time.time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (completed)
+            return;
+
+        float t = Time.time - startTime;
+
+        string minutes = ((int) t / 60).ToString();
+        string seconds = (t % 60).ToString("00");
+
+        timerText.text = minutes + ":" + seconds;
+    }
+
+    public void Completed()
+    {
+        completed = true;
+
+        string time = timerText.text;
+        finalTime.text = time;
+    }
+}
