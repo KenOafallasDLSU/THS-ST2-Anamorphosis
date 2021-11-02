27345f227850aa541e30f773c9811749d92f3728
LAPTOP-U61E0LQE\Kenneth <kenneth_oafallas@dlsu.edu.ph>
2021-11-02 14:18:25 +0800

    working check algo and mobile config

--- Assets/Scripts/Testing/AngleTracker.cs
+++ Assets/Scripts/Testing/AngleTracker.cs
@@ -8,27 +8,59 @@ public class AngleTracker : MonoBehaviour
 {
     [SerializeField] private GameObject camera;
     [SerializeField] private GameObject target;
+
+    [SerializeField] private float correctDist;
+    [SerializeField] private float distanceError;
+
+    [SerializeField] private float correctXAngle;
+    [SerializeField] private float correctYAngle;
+    [SerializeField] private float correctDelta;
+    [SerializeField] private float angleError;
+
     [SerializeField] private Text camText;
     [SerializeField] private Text targetText;
     [SerializeField] private Text distanceText;
     [SerializeField] private Text camAngText;
     [SerializeField] private Text tarAngText;
+    [SerializeField] private Text deltaAngText;
+    [SerializeField] private Text goodText;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        //add checking whether actively tracking
     }
 
     // Update is called once per frame
     void Update()
     {
+        //add condition of actively tracking
+
+        //store values for easier referencing
         float dist = Vector3.Distance(camera.transform.position, target.transform.position);
-        
+        float delta = Vector3.Angle(camera.transform.forward, target.transform.position - camera.transform.position);
+        float x = target.transform.localRotation.eulerAngles.x;
+        float y = target.transform.localRotation.eulerAngles.y;
+
+        bool goodDist = dist > correctDist - distanceError && dist < correctDist + distanceError;
+        bool goodAngle = delta > correctDelta - angleError && delta < correctDelta + angleError;
+        bool goodX = x > correctXAngle - angleError && x < correctXAngle + angleError;
+        bool goodY = y > correctYAngle - angleError && y < correctYAngle + angleError;
+
+        if(goodDist && goodAngle && goodX && goodY){
+            EventBroadcaster.Instance.PostEvent(EventNames.Anamorphosis_Events.ON_WIN);
+            goodText.text = "GOOD!!!";
+        }
+        else{
+            goodText.text = "NOT GOOD";
+        }
+
+        //update Test UI
         camText.text = "Cam Pos: " + camera.transform.position.ToString();
         targetText.text = "Tar Pos: " + target.transform.position.ToString();
         distanceText.text = "DIST: " + dist.ToString();
-        camAngText.text = "Cam Ang: " + camera.transform.localRotation.eulerAngles.ToString();
-        tarAngText.text = "Tar Ang: " + target.transform.localRotation.eulerAngles.ToString();
+        camAngText.text = "Cam Ang X: " + camera.transform.localRotation.eulerAngles.x.ToString() + " , Y: " + camera.transform.localRotation.eulerAngles.y.ToString();
+        tarAngText.text = "Tar Ang X: " + target.transform.localRotation.eulerAngles.x.ToString() + " , Y: " + target.transform.localRotation.eulerAngles.y.ToString();
+        deltaAngText.text = "DELTA: " + delta.ToString();
     }
 }
