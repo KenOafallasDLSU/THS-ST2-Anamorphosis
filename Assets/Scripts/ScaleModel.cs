d3312ef64051c6c6eda88374b6d3215f9213bf8e
Thea Go <theaellengo@gmail.com>
2021-11-22 15:44:56 +0800

    scale and change position dynamically

--- Assets/Scripts/ScaleModel.cs
+++ Assets/Scripts/ScaleModel.cs
@@ -15,15 +15,15 @@ public class ScaleModel : MonoBehaviour
 
     void Start()
     {
-        this.markerdist = target1.transform.position.z - target2.transform.position.z;
-        this.scale = 1.5f + (float)System.Math.Round(markerdist, 1); // more than one becomes jittery
-        this.transform.position = this.transform.position * scale;
-        model.transform.localScale = new Vector3(scale, scale, scale);
-        model.transform.position = model.transform.position + new Vector3(0, 0, -(this.markerdist+0.1125f)/2f);
     }
 
     // Update is called once per frame
     void Update()
     {
+        this.markerdist = Mathf.Abs(target1.transform.position.z - target2.transform.position.z);
+        this.scale = 1f + (float)System.Math.Round(markerdist, 1); // more than one becomes jittery
+        this.transform.position = this.transform.position * scale;
+        model.transform.localScale = new Vector3(scale, scale, scale);
+        model.transform.position = (target1.transform.position + target2.transform.position) / 2f;
     }
 }
