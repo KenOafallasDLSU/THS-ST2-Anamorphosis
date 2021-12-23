094e4b93ff28bc42c077971d2a45ea70be996ca8
michaeladizon <michaela_dizon@dlsu.edu.ph>
2021-12-24 02:27:16 +0800

    assets for ui

--- Assets/Scripts/GameProper/GameUI.cs
+++ Assets/Scripts/GameProper/GameUI.cs
@@ -5,11 +5,10 @@ using UnityEngine.UI;
 
 public class GameUI : MonoBehaviour
 {
-    [SerializeField] private Button instructions;
-    [SerializeField] private Image inst_msg;
-    [SerializeField] private Button x;
-    [SerializeField] private Button box;
-    [SerializeField] private Button nobox;
+    [SerializeField] private Button helpBtn;
+    [SerializeField] private Button onBtn;
+    [SerializeField] private Button offBtn;
+    [SerializeField] private Image instructions;
 
     [SerializeField] private Canvas modal;
     [SerializeField] private Text timerText;
@@ -17,41 +16,40 @@ public class GameUI : MonoBehaviour
 
     void Start()
     {
-        inst_msg.gameObject.SetActive(false);
-        x.gameObject.SetActive(false);
+        instructions.gameObject.SetActive(false);
+        modal.gameObject.SetActive(false);
+        offBtn.gameObject.SetActive(false);
     }
 
     public void showSelection()
     {
-        instructions.gameObject.SetActive(false);
-        nobox.gameObject.SetActive(false);
+        helpBtn.gameObject.SetActive(false);
+        offBtn.gameObject.SetActive(false);
     }
 
     public void showInst()
     {
-        instructions.gameObject.SetActive(false);
-        inst_msg.gameObject.SetActive(true);
-        x.gameObject.SetActive(true);
+        helpBtn.gameObject.SetActive(false);
+        instructions.gameObject.SetActive(true);
     }
 
     public void closeInst()
     {
-        instructions.gameObject.SetActive(true);
-        inst_msg.gameObject.SetActive(false);
-        x.gameObject.SetActive(false);
+        helpBtn.gameObject.SetActive(true);
+        instructions.gameObject.SetActive(false);
     }
 
     public void showBox()
     {
-        box.gameObject.SetActive(true);
-        nobox.gameObject.SetActive(false);
+        onBtn.gameObject.SetActive(true);
+        offBtn.gameObject.SetActive(false);
         boundingBox.gameObject.SetActive(true);
     }
 
     public void hideBox()
     {
-        box.gameObject.SetActive(false);
-        nobox.gameObject.SetActive(true);
+        onBtn.gameObject.SetActive(false);
+        offBtn.gameObject.SetActive(true);
         boundingBox.gameObject.SetActive(false);
     }
 }
